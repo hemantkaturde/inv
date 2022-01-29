d035ff5de61a2a577e94f53b57b93c221c3e3a93
Snehal More <snehasmore3@gmail.com>
2022-01-29 14:15:58 +0530

    pdf chaned

--- application/controllers/Controller_Tcpdf.php
+++ application/controllers/Controller_Tcpdf.php
@@ -16,18 +16,19 @@ class Controller_Tcpdf extends CI_Controller {
 
 		$inquiry_data = $this->Model_inquiry->getInquiryCustomerData($_SESSION['company_id'],$id);
 
-
+		// echo "<pre>";
 		// print_r($inquiry_data);
 		//  exit;
 		$company_name = $inquiry_data[0]['company_name'];
 		$company_address = $inquiry_data[0]['company_address'];
+		$company_phone = $inquiry_data[0]['phone'];
+		$company_email = $inquiry_data[0]['email1'];
 		$company_factory_address = $inquiry_data[0]['company_factory_address'];
 		
 
 		$this->load->library('MYPDF');
 
-		$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'utf-8', false);		
-		
+		$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'utf-8', false);
 
 		$pdf->SetCreator(PDF_CREATOR);
 		$pdf->SetAuthor('PdfWithCodeigniter');
@@ -45,7 +46,7 @@ class Controller_Tcpdf extends CI_Controller {
 
 		$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-		$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+		$pdf->SetMargins(10, 30, 10);
 		$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 		$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
@@ -56,22 +57,146 @@ class Controller_Tcpdf extends CI_Controller {
 		$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
 		// set font
-		$pdf->SetFont('times', 'BI', 12);
+		// $pdf->SetFont('times', 'BI', 12);
+		$pdf->SetFont('times', '', 11, '');
 		
 		// add a page
 		$pdf->AddPage();
 
+		$ref_no = $inquiry_data[0]['inquiry_number'];
+		$date = date('F j, Y', strtotime($inquiry_data[0]['inquiry_date']));
+		$name = $inquiry_data[0]['contact_person'];
+		$address = $inquiry_data[0]['address'];
+		$email = $inquiry_data[0]['email'];
+		$mobile = $inquiry_data[0]['mobile'];
+		$phone = $inquiry_data[0]['phone'];
+		$gst_no = $inquiry_data[0]['gst_no'];
 		// set some text to print
-		$txt = <<<EOD
-		TCPDF Codeigniter Example
-
-		Custom page header and footer are defined by extending the TCPDF class and overriding the Header() and Footer() methods.
+		$txt = "";
+		$txt .= <<<EOD
+			<table cellspacing="0" cellpadding="1" border="0">
+    			<tr>
+        			<td><b>Factory</b> : $company_address</td>
+					<td><b>Regd. Office</b> : $company_factory_address<br/>$company_phone<br/>$company_email</td>
+    			</tr>
+				<tr>
+        			<td>REF : $ref_no</td>
+					<td style="text-align:right">$date</td>
+    			</tr>
+			</table>
+EOD;
+		$txt .= <<<EOD
+		<table cellspacing="0" cellpadding="2">
+			<p>
+				To<br/>
+				$name<br/>$address<br/>Email : $email<br/>Phone : $phone / $mobile<br/> GST : $gst_no
+			</p>
+			<br/>
+				<p style="text-align:center;font-weight:bold;text-decoration: underline;">Quotation</p>
+			
+		</table>
+EOD;
+		$txt .= <<<EOD
+		<table cellspacing="0" cellpadding="2" border="1">
+			<tr>
+				<th>Description of Material</th>
+				<th>MOQ nos</th>
+				<th>Unit</th>
+				<th>Unit Price (Rs)/- nett</th>
+				<th>Amount (Rs)</th>
+			</tr>
+		</table>
+EOD;
+	$i =1;
+	$total_rate = 0;
+	$total_qty = 0;
+	$total_final_amt = 0;
+	foreach($inquiry_data as $key => $value):
+		$name = isset($value['name']) ? $value['name'] : "";
+		$qty = isset($value['qty']) ? $value['qty'] : 1;
+		$rate = isset($value['rate']) ? $value['rate'] : "";
+		$final_amount = isset($value['final_amount']) ? $value['final_amount'] : "";
+		
+		$txt .= <<<EOD
+		<table cellspacing="0" cellpadding="4" border="1">
+			<tr>
+				<td>$name</td>
+				<td>$qty</td>
+				<td>Nos</td>
+				<td>$rate</td>
+				<td>$final_amount</td>
+			</tr>
+		</table>
+EOD;
+	$i++;
+	$total_rate = $rate + $total_rate;
+	$total_qty = $qty + $total_qty;
+	$total_final_amt = $final_amount + $total_final_amt;
+	endforeach;
+	$gst = ($total_final_amt + 500) * (18/100);
+	$total = $total_final_amt+500+$gst;
+		$txt .= <<<EOD
+		<table cellspacing="0" cellpadding="4" border="1">
+			<tr>
+				<td>+ Freight</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td>500</td>
+			</tr>
+			<tr>
+				<td>+ GST 18%</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td>$gst</td>
+			</tr>
+			<tr>
+				<td>TOTAL</td>
+				<td>$total_qty</td>
+				<td></td>
+				<td>$total_rate</td>
+				<td>$total</td>
+			</tr>
+		</table>
+EOD;
+		
+$txt .= <<<EOD
+<table cellspacing="0" cellpadding="2"><br/><br/><b>Term & Conditions</b><br/></table>
 EOD;
 
-		// print a block of text using Write()
-		$pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
-		// $pdf->write($txt, true, false, false, false, '');
+$txt .= <<<EOD
+<table cellspacing="0" cellpadding="2">
+	<tr>
+		<td style="width:15%;text-decoration:underline">Delivery :</td>
+		<td style="width:95%">immediate on receipt of payment</td>
+	</tr>
+	<tr>
+		<td style="width:15%;text-decoration:underline">Freight :</td>
+		<td style="width: 95%">By XPS on freight paid basis</td>
+	</tr>
+	<tr>
+		<td style="width:15%;text-decoration:underline">Payment</td>
+		<td style="width:95%">
+			By Neft / RTGS<br/>
+			Union Bank of India - 21 Veena Chambers, Dalal St, Fort branch (Br No 24) Mumbai - 400023<br/>
+			Account: Hitech Electrocomponent Pvt Ltd<br/>
+			A / C No: 560101000107110 &nbsp;&nbsp;&nbsp;&nbsp; (current a/c)<br/>
+			RTGS No: UBIN0902217
+		</td>
+	</tr>
+	<tr>
+		<td style="width:15%;text-decoration:underline">Our GSTIN :</td>
+		<td style="width:95%">29AACCH1761N1Z8</td>
+	</tr>
+	<tr>
+	<td style="width:100%"><br/><br/>For Hitech Electrocomponents Pvt Ltd<br/><br/>Sd/-<br/><br/>Satish Shenoy</td>
+	</tr>
+</table>
+EOD;
 
+		// $pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
+		$pdf->writeHTML($txt, true, false, false, false, 'J');
 		// ---------------------------------------------------------
 		 ob_clean();
 		//Close and output PDF document
